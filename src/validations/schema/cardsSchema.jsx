d78f26ee8b89ebb7106b0b03d12068a0d56b5ae2
sourabhsjadhav99 <sourabhsjadhav99@gmail.com>
2024-04-16 15:41:16 +0530

    validations updated

--- src/validations/schema/cardsSchema.jsx
+++ src/validations/schema/cardsSchema.jsx
@@ -6,7 +6,7 @@ import * as Yup from "yup";
 const cardsSchema = Yup.object().shape({
   groupName: Yup.string().max(20, "Group Name Must be less than 20 characters").min(3, "Group Name Must be More Than 2 Characters").required('Group name is required'),
   groupImage: Yup.mixed(),
-  groupDescription: Yup.string().max(300, "Group Description Must be less than 300 characters").min(20, "Group Name Must be More Than 20 Characters").required('Group description is required'),
+  groupDescription: Yup.string().max(300, "Group Description Must be less than 300 characters").min(20, "Group Description Must be More Than 20 Characters").required('Group description is required'),
   cards: Yup.array().of(
     Yup.object().shape({
       name: Yup.string().max(20, "Card name Must be less than 20 characters").min(3, "Card name Must be More Than 2 Characters").required('Card name is required'),
